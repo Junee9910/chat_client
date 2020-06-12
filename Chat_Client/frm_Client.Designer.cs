8afd000b3a5210708a6c9730f713176a64042485
JuneeDinh <dinhttnhung9910@gmail.com>
2020-06-12 14:41:08 +0700

    Change...

---
+++ Chat_Client/frm_Client.Designer.cs
@@ -0,0 +1,277 @@
+﻿namespace Chat_Client
+{
+    partial class frm_Client
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.tbMess = new System.Windows.Forms.TextBox();
+            this.btSend = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btStart = new System.Windows.Forms.Button();
+            this.tbPortY = new System.Windows.Forms.TextBox();
+            this.tbIPY = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.tbPortYF = new System.Windows.Forms.TextBox();
+            this.tbIPYF = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.backgroundWorker2 = new System.ComponentModel.BackgroundWorker();
+            this.tbChatScreen = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.tbUserName = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(26, 695);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(72, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Messages";
+            // 
+            // tbMess
+            // 
+            this.tbMess.Location = new System.Drawing.Point(29, 726);
+            this.tbMess.Multiline = true;
+            this.tbMess.Name = "tbMess";
+            this.tbMess.Size = new System.Drawing.Size(665, 47);
+            this.tbMess.TabIndex = 2;
+            // 
+            // btSend
+            // 
+            this.btSend.Location = new System.Drawing.Point(717, 726);
+            this.btSend.Name = "btSend";
+            this.btSend.Size = new System.Drawing.Size(111, 47);
+            this.btSend.TabIndex = 3;
+            this.btSend.Text = "Send";
+            this.btSend.UseVisualStyleBackColor = true;
+            this.btSend.Click += new System.EventHandler(this.btSend_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btStart);
+            this.groupBox1.Controls.Add(this.tbPortY);
+            this.groupBox1.Controls.Add(this.tbIPY);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Location = new System.Drawing.Point(29, 111);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(799, 77);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Your Information";
+            // 
+            // btStart
+            // 
+            this.btStart.Location = new System.Drawing.Point(702, 24);
+            this.btStart.Name = "btStart";
+            this.btStart.Size = new System.Drawing.Size(75, 41);
+            this.btStart.TabIndex = 4;
+            this.btStart.Text = "Start";
+            this.btStart.UseVisualStyleBackColor = true;
+            this.btStart.Click += new System.EventHandler(this.btStart_Click);
+            // 
+            // tbPortY
+            // 
+            this.tbPortY.Location = new System.Drawing.Point(452, 30);
+            this.tbPortY.Name = "tbPortY";
+            this.tbPortY.Size = new System.Drawing.Size(188, 22);
+            this.tbPortY.TabIndex = 3;
+            // 
+            // tbIPY
+            // 
+            this.tbIPY.Location = new System.Drawing.Point(122, 33);
+            this.tbIPY.Name = "tbIPY";
+            this.tbIPY.Size = new System.Drawing.Size(188, 22);
+            this.tbIPY.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(371, 33);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(34, 17);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Port";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(31, 33);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(20, 17);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "IP";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.button3);
+            this.groupBox2.Controls.Add(this.tbPortYF);
+            this.groupBox2.Controls.Add(this.tbIPYF);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.label5);
+            this.groupBox2.Location = new System.Drawing.Point(29, 211);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(799, 77);
+            this.groupBox2.TabIndex = 5;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Your Friend\'s Information";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(702, 24);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 41);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Connect";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // tbPortYF
+            // 
+            this.tbPortYF.Location = new System.Drawing.Point(452, 30);
+            this.tbPortYF.Name = "tbPortYF";
+            this.tbPortYF.Size = new System.Drawing.Size(188, 22);
+            this.tbPortYF.TabIndex = 3;
+            // 
+            // tbIPYF
+            // 
+            this.tbIPYF.Location = new System.Drawing.Point(122, 33);
+            this.tbIPYF.Name = "tbIPYF";
+            this.tbIPYF.Size = new System.Drawing.Size(188, 22);
+            this.tbIPYF.TabIndex = 2;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(371, 33);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(34, 17);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "Port";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(31, 33);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(20, 17);
+            this.label5.TabIndex = 0;
+            this.label5.Text = "IP";
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            // 
+            // backgroundWorker2
+            // 
+            this.backgroundWorker2.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker2_DoWork);
+            // 
+            // tbChatScreen
+            // 
+            this.tbChatScreen.Location = new System.Drawing.Point(29, 309);
+            this.tbChatScreen.Multiline = true;
+            this.tbChatScreen.Name = "tbChatScreen";
+            this.tbChatScreen.Size = new System.Drawing.Size(799, 374);
+            this.tbChatScreen.TabIndex = 6;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(256, 51);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(38, 17);
+            this.label6.TabIndex = 7;
+            this.label6.Text = "User";
+            // 
+            // tbUserName
+            // 
+            this.tbUserName.Location = new System.Drawing.Point(376, 48);
+            this.tbUserName.Multiline = true;
+            this.tbUserName.Name = "tbUserName";
+            this.tbUserName.ReadOnly = true;
+            this.tbUserName.Size = new System.Drawing.Size(116, 30);
+            this.tbUserName.TabIndex = 8;
+            // 
+            // frm_Client
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(859, 785);
+            this.Controls.Add(this.tbUserName);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.tbChatScreen);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.btSend);
+            this.Controls.Add(this.tbMess);
+            this.Controls.Add(this.label1);
+            this.Name = "frm_Client";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.frm_Client_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tbMess;
+        private System.Windows.Forms.Button btSend;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btStart;
+        private System.Windows.Forms.TextBox tbPortY;
+        private System.Windows.Forms.TextBox tbIPY;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox tbPortYF;
+        private System.Windows.Forms.TextBox tbIPYF;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker2;
+        private System.Windows.Forms.TextBox tbChatScreen;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox tbUserName;
+    }
+}
+
---
+++ Chat_Client/frm_Login.Designer.cs
@@ -0,0 +1,135 @@
+﻿namespace Chat_Client
+{
+    partial class frm_Login
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lbPass = new System.Windows.Forms.Label();
+            this.tbUser = new System.Windows.Forms.TextBox();
+            this.tbPass = new System.Windows.Forms.TextBox();
+            this.btDangNhap = new System.Windows.Forms.Button();
+            this.btCancel = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.label1.Location = new System.Drawing.Point(241, 58);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(78, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Đăng nhập";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(71, 111);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(71, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Tài khoản";
+            // 
+            // lbPass
+            // 
+            this.lbPass.AutoSize = true;
+            this.lbPass.Location = new System.Drawing.Point(71, 171);
+            this.lbPass.Name = "lbPass";
+            this.lbPass.Size = new System.Drawing.Size(66, 17);
+            this.lbPass.TabIndex = 2;
+            this.lbPass.Text = "Mật khẩu";
+            // 
+            // tbUser
+            // 
+            this.tbUser.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.tbUser.Location = new System.Drawing.Point(191, 108);
+            this.tbUser.Multiline = true;
+            this.tbUser.Name = "tbUser";
+            this.tbUser.Size = new System.Drawing.Size(274, 42);
+            this.tbUser.TabIndex = 3;
+            // 
+            // tbPass
+            // 
+            this.tbPass.Location = new System.Drawing.Point(191, 168);
+            this.tbPass.Multiline = true;
+            this.tbPass.Name = "tbPass";
+            this.tbPass.Size = new System.Drawing.Size(274, 36);
+            this.tbPass.TabIndex = 4;
+            // 
+            // btDangNhap
+            // 
+            this.btDangNhap.Location = new System.Drawing.Point(153, 246);
+            this.btDangNhap.Name = "btDangNhap";
+            this.btDangNhap.Size = new System.Drawing.Size(96, 33);
+            this.btDangNhap.TabIndex = 5;
+            this.btDangNhap.Text = "Đăng nhập";
+            this.btDangNhap.UseVisualStyleBackColor = true;
+            this.btDangNhap.Click += new System.EventHandler(this.btDangNhap_Click);
+            // 
+            // btCancel
+            // 
+            this.btCancel.Location = new System.Drawing.Point(348, 246);
+            this.btCancel.Name = "btCancel";
+            this.btCancel.Size = new System.Drawing.Size(75, 33);
+            this.btCancel.TabIndex = 6;
+            this.btCancel.Text = "Cancel";
+            this.btCancel.UseVisualStyleBackColor = true;
+            this.btCancel.Click += new System.EventHandler(this.btCancel_Click);
+            // 
+            // frm_Login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(599, 358);
+            this.Controls.Add(this.btCancel);
+            this.Controls.Add(this.btDangNhap);
+            this.Controls.Add(this.tbPass);
+            this.Controls.Add(this.tbUser);
+            this.Controls.Add(this.lbPass);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "frm_Login";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "frm_Login";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lbPass;
+        private System.Windows.Forms.TextBox tbUser;
+        private System.Windows.Forms.TextBox tbPass;
+        private System.Windows.Forms.Button btDangNhap;
+        private System.Windows.Forms.Button btCancel;
+    }
+}
